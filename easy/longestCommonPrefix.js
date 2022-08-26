f980e5fbf59311aed27fb39288dff1bc7d6ab84c
David Mott <d.s.mott0808@gmail.com>
2022-08-26 13:22:12 -0700

    add file

---
+++ easy/longestCommonPrefix.js
@@ -0,0 +1,16 @@
+function longestCommonPrefix(strs) {
+    if (strs.length === 0) return '';
+    const first = strs[0];
+  
+    for (let i = 0; i < first.length; i++) {
+      for (let j = 1; j < strs.length; j++) {
+        const other = strs[j];
+        if (other[i] !== first[i]) {
+          return other.slice(0, i);
+        }
+      }
+    }
+  
+    return first;
+  }
+longestCommonPrefix(["flower", "flow", "flight"])
