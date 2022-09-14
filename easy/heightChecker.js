6a97160e0be4d6c52d3816eb05a680885930e143
David Mott <d.s.mott0808@gmail.com>
2022-09-14 11:09:00 -0700

    add file

--- easy/heightChecker.js
+++ easy/heightChecker.js
@@ -1,4 +1,4 @@
-heightChecker = (heights) => {
+let heightChecker = (heights) => {
   const sorted = [...heights].sort((a,b) => a-b,0)
   let outOfOrder = 0;
   for(let i = 0; i < heights.length; i++){
@@ -8,3 +8,5 @@ heightChecker = (heights) => {
   }
   return outOfOrder
 };
+
+console.log(heightChecker([1,1,4,2,1,3]))
